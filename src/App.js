8b4a647149b2f4722340783605a6b7c1683fd7ff
ramchandrabobhate <ramchandrabobhate@gmail.com>
2022-03-24 17:16:21 +0530

    APP.js
    Hardcore key

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
       // const proxy='https://cors-anywhere.herokuapp.com/';
      // const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${categories}&apiKey=${process.env.REACT_APP_API_KEY}`)
      
-       const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${categories}&apiKey=${process.env.REACT_APP_API_KEY}`)
+      // const news = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&category=${categories}&apiKey=${process.env.REACT_APP_API_KEY}`)
+      const news=awit axios.get('https://newsapi.org/v2/top-headlines?country=in&category=sport&apiKey=99c8c899198b4be29373812cf205301d')
         console.log(news.data);
       setNewsArr(news.data.articles)
       setnewsresult(news.data.totalResults)
